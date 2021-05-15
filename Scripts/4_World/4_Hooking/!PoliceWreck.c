d854f44094e94e29659bdd5ab2c392bf034483eb
philippj <philipp98.joos@gmail.com>
2021-05-15 17:59:52 +0200

    Add all police wreck types

--- Scripts/4_World/4_Hooking/!PoliceWreck.c
+++ Scripts/4_World/4_Hooking/!PoliceWreck.c
@@ -16,3 +16,79 @@ class Land_Wreck_hb01_aban1_police extends House {
         if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
     }
 };
+
+class Land_Wreck_hb01_aban2_police extends House {
+        ref _Event _registeredInstance = new _Event(this.GetType(), "car", this);
+
+        override void EEInit() {
+            super.EEInit();
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            vector position = GetPosition();
+            if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+            GetGameLabs().RegisterEvent(this._registeredInstance);
+        }
+        override void EEDelete(EntityAI parent) {
+            super.EEDelete(parent);
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        }
+};
+
+class Land_Wreck_sed01_aban1_police extends House {
+        ref _Event _registeredInstance = new _Event(this.GetType(), "car", this);
+
+        override void EEInit() {
+            super.EEInit();
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            vector position = GetPosition();
+            if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+            GetGameLabs().RegisterEvent(this._registeredInstance);
+        }
+        override void EEDelete(EntityAI parent) {
+            super.EEDelete(parent);
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        }
+};
+
+class Land_Wreck_sed01_aban2_police extends House {
+        ref _Event _registeredInstance = new _Event(this.GetType(), "car", this);
+
+        override void EEInit() {
+            super.EEInit();
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            vector position = GetPosition();
+            if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+            GetGameLabs().RegisterEvent(this._registeredInstance);
+        }
+        override void EEDelete(EntityAI parent) {
+            super.EEDelete(parent);
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        }
+};
+
+class Land_Wreck_Volha_Police extends House {
+        ref _Event _registeredInstance = new _Event(this.GetType(), "car", this);
+
+        override void EEInit() {
+            super.EEInit();
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            vector position = GetPosition();
+            if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+            GetGameLabs().RegisterEvent(this._registeredInstance);
+        }
+        override void EEDelete(EntityAI parent) {
+            super.EEDelete(parent);
+            if(!GetGame().IsServer()) return;
+            if(!GetGameLabs()) return;
+            if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        }
+};
