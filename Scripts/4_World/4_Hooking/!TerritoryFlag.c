e15575493428d9ceb60902082f03170fd1c3c856
philippj <philipp98.joos@gmail.com>
2021-05-16 12:27:10 +0200

    Track territory flag on map

--- Scripts/4_World/4_Hooking/!TerritoryFlag.c
+++ Scripts/4_World/4_Hooking/!TerritoryFlag.c
@@ -1,17 +1,15 @@
 modded class TerritoryFlag {
-    ref _Event _registeredInstance = new _Event(this.GetType(), "flag-alt", this);
+    ref _Event _registeredInstance = new _Event("TerritoryFlag", "flag", this);
 
     void TerritoryFlag () {
-        if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
-        vector position = GetPosition();
-        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        if(!GetGameLabs().IsServer()) return;
         GetGameLabs().RegisterEvent(this._registeredInstance);
     }
 
     void ~TerritoryFlag () {
-        if(!GetGame().IsServer()) return;
         if(!GetGameLabs()) return;
+        if(!GetGameLabs().IsServer()) return;
         if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
     }
 };
