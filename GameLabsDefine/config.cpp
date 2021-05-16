db44940159b0db0b878d4a8e340a9fa78a128a7d
philippj <philipp98.joos@gmail.com>
2021-05-16 19:08:41 +0200

    Define fixes

--- GameLabsDefine/config.cpp
+++ GameLabsDefine/config.cpp
@@ -1,3 +1,5 @@
+#define GAMELABS
+
 class CfgPatches
 {
     class GameLabsDefine
--- Scripts/Workbench/Batchfiles/BinarizePBO.bat
+++ Scripts/Workbench/Batchfiles/BinarizePBO.bat
@@ -204,7 +204,7 @@ del %modBuildDirectory%%modName%\Addons\!pboName!.pbo.%keyName%.bisign
 
 echo Building PBO: !pboName!.pbo
 echo START /w %pboProject% %pboProject% +W "+^!" -F +Stop -P +Z +O -E=dayz +R "%workDrive%!prefixName!" "+Mod=%modBuildDirectory%%modName%" "+Key=F:\@DayZ\@Keys\cftoolsRoot.biprivatekey"
-echo ADD "+^!" AND +O to obfuscate
+echo ADD "+^!" +O to obfuscate
 START /w %pboProject% %pboProject% +K +W -F +Stop -P +Z "+^!" +O -E=dayz +R "%workDrive%!prefixName!" "+Mod=%modBuildDirectory%%modName%" "+Key=F:\@DayZ\@Keys\cftoolsRoot.biprivatekey"
 
 if not errorlevel 1 (
