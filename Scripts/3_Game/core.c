c97179482709774efdb754414dc9c27a19be984d
philippj <philipp98.joos@gmail.com>
2021-05-16 18:09:34 +0200

    Version bump for upcoming breaking api changes

--- Scripts/3_Game/core.c
+++ Scripts/3_Game/core.c
@@ -1,5 +1,5 @@
 class GameLabsCore {
-    private const string modControlledVersionIdentifier = "1.1";
+    private const string modControlledVersionIdentifier = "1.2";
 
     private ref GameLabsAPI api;
     private ref GameLabsLogger logger;
