85a59ec7964fab421533f61ed0502dab3c662212
philippj <philipp98.joos@gmail.com>
2021-05-18 13:31:51 +0200

    Rework registration logic in case return may lead to issues in constructor/destructor

--- Scripts/4_World/4_Hooking/!TerritoryFlag.c
+++ Scripts/4_World/4_Hooking/!TerritoryFlag.c
@@ -2,15 +2,19 @@ modded class TerritoryFlag {
     ref _Event _registeredInstance = new _Event("TerritoryFlag", "pennant", this);
 
     void TerritoryFlag () {
-        if(!GetGameLabs()) return;
-        if(!GetGameLabs().IsServer()) return;
-        GetGameLabs().RegisterEvent(this._registeredInstance);
+        if(GetGameLabs()) {
+            if(GetGameLabs().IsServer()) {
+                GetGameLabs().RegisterEvent(this._registeredInstance);
+            }
+        }
     }
 
     void ~TerritoryFlag () {
-        if(!GetGameLabs()) return;
-        if(!GetGameLabs().IsServer()) return;
-        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+        if(GetGameLabs()) {
+            if(GetGameLabs().IsServer()) {
+                if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+            }
+        }
     }
 
     // Public API
