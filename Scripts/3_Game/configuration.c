b85d87a50cc2ab0e3c5357e6ae453871778f5b69
philippj <philipp98.joos@gmail.com>
2021-05-27 17:11:33 +0200

    Configuration options for speed check

--- Scripts/3_Game/configuration.c
+++ Scripts/3_Game/configuration.c
@@ -8,6 +8,12 @@ class GameLabsConfiguration {
     private string baseUrl = "https://api.cftools.cloud/gamelabs/dz";
     private string storeUrl = "https://api.cftools.cloud/gamelabs/dz";
 
+    private bool speedCheckEnabled = true;
+    private float speedCheckThresholdFoot = 8.0;
+    private float speedCheckThresholdVehicle = 100.0;
+
+    private float playerTickInterval = 2.5;
+
     [NonSerialized()]
     private const string path = "$profile:gamelabs.cfg";
 
@@ -21,7 +27,14 @@ class GameLabsConfiguration {
 
     void LoadFromDisk() {
         JsonFileLoader<GameLabsConfiguration>.JsonLoadFile(this.path, this);
+        this._ValidateAndExtend();
     }
+
+    float GetPlayerTickInterval() { return this.playerTickInterval; }
+
+    bool GetSpeedCheckStatus() { return this.speedCheckEnabled; }
+    float GetSpeedCheckThresholdFoot() { return this.speedCheckThresholdFoot; }
+    float GetSpeedCheckThresholdVehicle() { return this.speedCheckThresholdVehicle; }
 
     string GetServerId() { return this.serverId; }
     string GetApiKey() { return this.apiKey; }
@@ -30,4 +43,10 @@ class GameLabsConfiguration {
     bool GetConnectionVerificationStatus() { return this.connectionVerification; }
     bool GetDebugStatus() { return this.debugEnabled; }
     void OverrideDebugStatus(bool debugEnabled) { this.debugEnabled = debugEnabled; }
+
+    void _ValidateAndExtend() {
+        if(this.speedCheckEnabled == NULL) this.speedCheckEnabled = true;
+        if(this.speedCheckThresholdFoot == 1) this.speedCheckThresholdFoot = 8.0;
+        if(this.speedCheckThresholdVehicle == 1) this.speedCheckThresholdVehicle = 100.0;
+    }
 };
