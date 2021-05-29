76218fa2cf5709615d8a3ed3662e13f0a0e4afa1
philippj <philipp98.joos@gmail.com>
2021-05-29 17:05:26 +0200

    Added comment detailing deferred loading strategy

--- Scripts/4_World/4_Hooking/!TerritoryFlag.c
+++ Scripts/4_World/4_Hooking/!TerritoryFlag.c
@@ -2,6 +2,8 @@ modded class TerritoryFlag extends BaseBuildingBase {
     ref _Event _registeredInstance;
 
     void TerritoryFlag () {
+        // This defers initialization of the GameLabs _Event as it probably breaks execution of other modded
+        // constructors because it uses a hard ref to the instance
         GetGame().GetCallQueue(CALL_CATEGORY_SYSTEM).Call(this._InitGameLabs);
 	}
 
