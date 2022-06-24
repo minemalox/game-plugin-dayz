bade8cdba19f43baa600c0f2c74db62cc017f742
minemalox <55745413+minemalox@users.noreply.github.com>
2022-06-24 19:07:19 +0200

    Added modded HeliCrashes

--- Scripts/4_World/4_Hooking/!HeliCrash.c
+++ Scripts/4_World/4_Hooking/!HeliCrash.c
@@ -52,6 +52,94 @@ modded class Wreck_Mi8_Crashed extends CrashBase {
     }
 };
 
+modded class crashed_Wreck_C130_Camo extends CrashBase {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "helicopter", this);
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
+
+modded class crashed_Wreck_C130J extends CrashBase {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "helicopter", this);
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
+
+modded class crashed_Wreck_Mi8_RU extends CrashBase {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "helicopter", this);
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
+modded class crashed_Wreck_Mi8_CDF extends CrashBase {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "helicopter", this);
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
+modded class crashed_Wreck_UH1Y extends CrashBase {
+    ref _Event _registeredInstance = new _Event(this.GetType(), "helicopter", this);
+
+    override void EEInit() {
+        super.EEInit();
+        if(!GetGame().IsServer()) return;
+        vector position = GetPosition();
+        if(position[0] <= 0 && position[1] <= 0 && position[2] <= 0) return;
+        GetGameLabs().RegisterEvent(this._registeredInstance);
+    }
+    override void EEDelete(EntityAI parent) {
+        super.EEDelete(parent);
+        if(!GetGame().IsServer()) return;
+        if(!GetGameLabs()) return;
+        if(this._registeredInstance) GetGameLabs().RemoveEvent(this._registeredInstance);
+    }
+};
+
 /*
  * TODO: Investigate crash issue
 class Wreck_SantasSleigh extends House {
